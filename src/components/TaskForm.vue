7605f2334ad84782ad75356196a17966013335af
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-02-12 19:02:50 -0600

    initialized Task form

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+const newTask = ref('');
+const emit = defineEmits(['add-task']);
+
+const addNewTask = () => {
+  if (newTask.value.trim()) {
+    emit('add-task', newTask.value);
+    newTask.value = '';
+  }
+};
+</script>
+
+<template>
+  <div>
+    <input v-model="newTask" placeholder="New task" @keyup.enter="addNewTask" />
+    <button @click="addNewTask">Add Task</button>
+  </div>
+</template>
+
