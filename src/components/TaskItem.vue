585341b00d43c4e5fb8a4f52119f795ab69c77f4
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-02-12 19:03:56 -0600

    added the task item and task list

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(['task']);
+const emit = defineEmits(['toggle-task', 'delete-task']);
+</script>
+
+<template>
+    <div class="list">
+        <li :class="{ completed: task.completed }" >
+            <span @click="emit('toggle-task', task.id)">{{ task.text }}</span>
+            <button class="deleteButton" @click="emit('delete-task', task.id)" >Delete</button>
+        </li>
+  </div>
+</template>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TaskItem from './TaskItem.vue';
+
+defineProps(['tasks']);
+const emit = defineEmits(['toggle-task', 'delete-task']);
+</script>
+
+<template>
+  <ul>
+    <TaskItem 
+      v-for="task in tasks" 
+      :key="task.id" 
+      :task="task" 
+      @toggle-task="emit('toggle-task', task.id)" 
+      @delete-task="emit('delete-task', task.id)" 
+    />
+  </ul>
+</template>
