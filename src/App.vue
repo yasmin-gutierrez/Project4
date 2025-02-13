5f7a9db90f942bb724b4077597db719dbc1e94b0
Yasmin Gutierrez <yasmin.gutierrez101@gmail.com>
2025-02-12 19:01:19 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,6 @@ import { ref, computed, onMounted, watch } from 'vue';
 import TaskList from './components/TaskList.vue';
 import TaskForm from './components/TaskForm.vue';
 
-const tasks = ref([]);
-
-const addTask = (taskText) => {
-  tasks.value.push({ id: Date.now(), text: taskText, completed: false });
-};
-
-const toggleTask = (taskId) => {
-  const task = tasks.value.find(t => t.id === taskId);
-  if (task) task.completed = !task.completed;
-};
-
-const deleteTask = (taskId) => {
-  tasks.value = tasks.value.filter(t => t.id !== taskId);
-};
-
-const remainingTasks = computed(() => tasks.value.filter(task => !task.completed).length);
 
 </script>
 
